93bd8e127ea0dab4fad71d1013463e86eacf5a16
Ali Umer <i210380@nu.edu.pk>
2025-04-18 14:54:02 +0500

    Chunked the text using langchain's text splitter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import fitz  # New name for PyMuPDF
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def extract_text_from_pdf(pdf_path):
     doc = fitz.open(pdf_path)
@@ -7,6 +8,28 @@ def extract_text_from_pdf(pdf_path):
         text += page.get_text()
     return text
 
-text = extract_text_from_pdf("Ali's part.pdf")
+# def chunk_text(text, chunk_size, overlap):# Function to chunk text into smaller pieces
+#     words = text.split()# Split text into words based on whitespace
+#     chunks = []
 
-print(text)
+#     for i in range(0, len(words), chunk_size - overlap): # Iterate over words with a step size of chunk_size - overlap
+#         chunk = ' '.join(words[i:i + chunk_size]) # Join the words in the current chunk
+#         chunks.append(chunk) # Append the chunk to a list
+    
+#     return chunks
+#chunks = chunk_text(text, 300, 50) #example usage of the chunk_text function
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+text = extract_text_from_pdf("Ali's part.pdf") # Store the extracted text in a variable
+chunks = text_splitter.split_text(text)
+
+
+print(f"Total chunks created: {len(chunks)}")
+
+for i in chunks:
+    print(i)
+    print("\n-------------------------------------------------------\n")
+
