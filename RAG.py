9fec9299f611ce7ddd2cd8fd19f886a17f420502
Ali Umer <i210380@nu.edu.pk>
2025-06-11 19:08:00 +0500

    uopdated backend

--- RAG.py
+++ RAG.py
@@ -7,7 +7,7 @@ import pickle
 
 INDEX_FILE = "output_data/faiss.index"
 CHUNKS_FILE = "output_data/chunks.pkl"
-PDF_FILE = "input_data/Prospectus 2024.pdf"
+PDF_FILE = "input_data/Faculty Retention Policy 2025 V1.0.docx"
 
 def extract_text_from_pdf(pdf_path):
     doc = fitz.open(pdf_path)
